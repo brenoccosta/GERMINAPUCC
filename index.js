2a73ec48427903e7e4b363e3947d9c1c23679853
Breno Coltro da Costa <brenocoltrodacosta@gmail.com>
2022-11-03 16:26:36 -0300

    Revisão geral de index.html e de index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// BRENO COLTRO DA COSTA
+// FERNANDO DE FACIO ROSSETTI
+// GIOVANE BRUNO NARDARI
+// LUIS HENRRIQUE CREPALDI MOLAS
+
+// INICIALIZAÇÃO
 let modal = document.getElementById("myModal");
 let btn = document.getElementById("conta");
 let span = document.getElementsByClassName("close")[0];
@@ -16,34 +22,32 @@ window.onclick = function(event) {
   }
 }
 
+// CARROSSEL DE NOTÍCIAS
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-//CARROSSEL DE NOTICIAS
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("demo");
+//   let captionText = document.getElementById("caption");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+//   captionText.innerHTML = dots[slideIndex-1].alt;
+// }
