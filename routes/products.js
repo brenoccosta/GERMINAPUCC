5be0dde019924df7e14e46a9de8075afca1a3a17
gnardari <giovanenardari@gmail.com>
2022-11-24 18:08:43 -0300

    ejs tags para a pagina de produtos

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
+const sqlite3 = require('sqlite3');
 
+let db = new sqlite3.Database('../../germina.db', (err) => {
+	if (err) {
+	  console.error(err.message);
+	}
+	console.log('Conectado a base de dados');
+});
 
-// FAZER QUERIES
 router.get('/', function(req, res) {
     req.session.returnTo = req.originalUrl;
-    res.render('produtos', {produtos: [a]});
+    res.render('produtos');
     
 });
 
