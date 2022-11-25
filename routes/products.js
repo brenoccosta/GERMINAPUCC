9f4ab1e69ba926f687489958aa99fc6bafad6732
Giovane Bruno Nardari <88439122+GiovaneNardari@users.noreply.github.com>
2022-11-25 09:51:01 -0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,17 +2,20 @@ const express = require('express');
 const router = express.Router();
 const sqlite3 = require('sqlite3');
 
-let db = new sqlite3.Database('../../germina.db', (err) => {
+let db = new sqlite3.Database('germina.db', (err) => {
 	if (err) {
 	  console.error(err.message);
 	}
-	console.log('Conectado a base de dados');
 });
+
+//query para receber os produtos com seus preços
+const pesquisaProdutos = require('../public/js/queryProduto.js');
+let listaProdutos = [];
+pesquisaProdutos.queryProdutos(db, listaProdutos);
 
 router.get('/', function(req, res) {
     req.session.returnTo = req.originalUrl;
-    res.render('produtos');
-    
+    res.render('produtos', {produtos: listaProdutos});
 });
 
 module.exports = router;
